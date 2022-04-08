b1bab71d0dd32ca0160d3c912ccf794fe62063c1
Samith-Ailapperuma <samithailapperuma@gmail.com>
2022-04-08 21:20:23 +0530

    Filter events based on project

--- Client/src/Components/AddProject.js
+++ Client/src/Components/AddProject.js
@@ -22,6 +22,7 @@ function AddProject() {
     }
 
     let navigate = useNavigate();
+    
     const navAllProjects = () => { navigate("/allProjects") }
 
     /* eslint-disable no-unused-expressions */
