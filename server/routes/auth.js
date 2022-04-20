0e24b718f86e854801ba1bb8928fa64d0ba52ec2
Zenith Anthony <dabarerawza.19@uom.lk>
2022-04-20 22:17:40 +0530

    verified added to data model

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,10 +8,16 @@ router.post("/", async (req, res) => {
 		const { error } = validate(req.body);
 		if (error)
 			return res.status(400).send({ message: error.details[0].message });
+			
 
 		const user = await User.findOne({ email: req.body.email });
 		if (!user)
 			return res.status(401).send({ message: "Invalid Email or Password" });
+		
+			if (!user.verified){
+				console.log("Restricted user");
+				return res.status(401).send({ message: "Invalid Email or Password" });
+		}
 
 		const validPassword = await bcrypt.compare(
 			req.body.password,
