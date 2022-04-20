4fa74b4929a92881a926a9ea959c30790efe89f7
Veenaka <lahiruveenaka@gmail.com>
2022-04-20 19:14:02 +0530

    Removed the Notifications Handling module from the Add Post Module

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,6 @@ import EditPost from "./components/EditPost";
 import Home from "./components/Home";
 import PostDetails from "./components/PostDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
-import RequesttoCollaborateProject from "./components/RequesttoCollaborateProject";
-import NotificationAddevent from "./components/Notificationaddevent";
-import RemovalOfAProject from "./components/RemovalOfAProject";
 
 export default class App extends Component {
   render() {
@@ -19,18 +16,6 @@ export default class App extends Component {
             <Route path="/add" element={<CreatePost />}></Route>
             <Route path="/edit/:id" element={<EditPost />}></Route>
             <Route path="/post/:id" element={<PostDetails />}></Route>
-            <Route
-              path="/collaborateProject"
-              element={<RequesttoCollaborateProject />}
-            ></Route>
-            <Route
-              path="/notificationaddevent"
-              element={<NotificationAddevent />}
-            ></Route>
-            <Route
-              path="/removalofaproject"
-              element={<RemovalOfAProject />}
-            ></Route>
           </Routes>
         </div>
       </BrowserRouter>
