d6cd47f0ff3426073a0c237f017187c444deb050
amashashalindi <desilvanas.19@uom.lk>
2022-04-21 00:35:42 +0530

    added view button

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,6 +1,9 @@
 import styles from "./styles.module.css";
+import { useNavigate } from 'react-router-dom';
+
 
 const Main = () => {
+	const navigate = useNavigate();
 	const handleLogout = () => {
 		localStorage.removeItem("token");
 		window.location.reload();
@@ -15,6 +18,11 @@ const Main = () => {
 				</button>
 				
 			</nav>
+			<button className="btn btn-success"
+                        onClick={() => {navigate(`/detail/`)}}
+                        >View
+                        
+                    </button>
 		</div>
 	);
 };
