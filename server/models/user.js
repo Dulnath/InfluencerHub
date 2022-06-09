7be307ee07cbfd7c6b17548059f263793fc703ad
Zenith Anthony <dabarerawza.19@uom.lk>
2022-06-09 22:37:17 +0530

    coloer edited

--- server/models/user.js
+++ server/models/user.js
@@ -4,44 +4,43 @@ const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
 
 const userSchema = new mongoose.Schema({
-	firstName: { type: String, required: false },
-	businessName: { type: String, required: false },
-	businessAddress: { type: String, required: false },
-	lastName: { type: String, required: false },
-	email: { type: String, required: true },
-	password: { type: String, required: true },
-	category:{ type: String, required: false },
-	verified:{type:Boolean,default:false},
-	isActive:{type:Boolean,default:true}
-
-	
+  firstName: { type: String, required: false },
+  businessName: { type: String, required: false },
+  businessAddress: { type: String, required: false },
+  lastName: { type: String, required: false },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  category: { type: String, required: false },
+  verified: { type: Boolean, default: false },
+  isActive: { type: Boolean, default: true },
 });
 
 userSchema.methods.generateAuthToken = function () {
-	const token = jwt.sign({ _id: this._id,category:this.category }, process.env.JWTPRIVATEKEY, {
-		expiresIn: "7d",
-	});
-	return token;
+  const token = jwt.sign(
+    { _id: this._id, category: this.category },
+    process.env.JWTPRIVATEKEY,
+    {
+      expiresIn: "7d",
+    }
+  );
+  return token;
 };
 
 const User = mongoose.model("user", userSchema);
 
 const validate = (data) => {
-	const schema = Joi.object({
-		firstName: Joi.string().label("First Name"),
-		businessName: Joi.string().label("Business Name"),
-		businessAddress: Joi.string().label("Business Address"),
-		lastName: Joi.string().label("Last Name"),
-		email: Joi.string().email().label("Email"),
-		password: passwordComplexity().label("Password"),
-		category: Joi.string().label("Category"),
-		verified:Joi.string().label("Verified"),
-		isActive:Joi.string().label("Active")
-	});
-	return schema.validate(data);
+  const schema = Joi.object({
+    firstName: Joi.string().label("First Name"),
+    businessName: Joi.string().label("Business Name"),
+    businessAddress: Joi.string().label("Business Address"),
+    lastName: Joi.string().label("Last Name"),
+    email: Joi.string().email().label("Email"),
+    password: passwordComplexity().label("Password"),
+    category: Joi.string().label("Category"),
+    verified: Joi.string().label("Verified"),
+    isActive: Joi.string().label("Active"),
+  });
+  return schema.validate(data);
 };
 
-module.exports = { User:User
-	, validate
-};
-
+module.exports = { User: User, validate };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,143 +1,15 @@
 const router = require("express").Router();
-const { User, validate } = require("../models/user");
-const bcrypt = require("bcrypt");
-const Token = require("../models/token");
-const sendEmail = require("../utils/sendEmail");
-const crypto = require("crypto");
-//signup
-router.post("/", async (req, res) => {
-	try {
-		const { error } = validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
+const {
+  AddUsers,
+  EmailVerify,
+  getById,
+  getUsers,
+} = require("../controller/users");
 
-		let user = await User.findOne({ email: req.body.email });
-		if (user)
-			return res
-				.status(409)
-				.send({ message: "User with given email already Exist!" });
+//user registration routes
+router.post("/", AddUsers);
+router.get("/getUsers", getUsers);
+router.get("/:id/verify/:token/", EmailVerify);
+router.get("/getuser/:id", getById);
 
-		const salt = await bcrypt.genSalt(Number(process.env.SALT));
-		const hashPassword = await bcrypt.hash(req.body.password, salt);
-	
-
-		let userRes = {
-			email: req.body.email,
-			password: hashPassword,
-			category:req.body.category
-		};
-	
-		if(req.body.category==="influencer")
-
-		{
-			userRes = {
-				...userRes,
-				firstName: req.body.firstName,
-				lastName: req.body.lastName,
-			}
-		}
-		else if(req.body.category==="business")
-		{
-			userRes = {
-				...userRes,
-				businessName: req.body.businessName,
-				businessAddress: req.body.businessAddress,
-			 }
-		}
-		//console.log("error")
-		 user =  await new User(userRes).save();
-		//await new User(userRes).save();
-		console.log("Print user")
-		console.log(user);
-		const token = await new Token({
-			userId: user._id,
-			token: crypto.randomBytes(32).toString("hex"),
-		}).save();
-		const url = `${process.env.BASE_URL}users/${user.id}/verify/${token.token}`;
-		 await sendEmail(user.email, "Verify Email", url);
-		res
-			.status(201)
-			.send({ message: "An Email sent to your account please verify" });
-	//	res.status(201).send({ message: "User created successfully" });
-	
-	} catch (error) {
-		console.log(error);
-		res.status(500).send({ message: "Internal Serversdfsadf Error",error });
-	}
-});
-
-
-router.get('/getUsers', (req, res) => {
-    User.find()
-        .sort({ date: -1 })
-        .then(items => res.json(items))
-});
-/*
-// get individual user
-router.get("/viewUsers/:id",(req,res) => {
-    
-    let userId = req.params.id;
-    User.findById(userId,(err,user) => {
-        if(err){
-            return res.status(400).json({success:false, err});
-        }
-        return res.status(200).json({
-            success:true,
-            user
-        });
-    });
-});*/
-
-// get individual user
-/*
-router.get(`/getuser/${id}`,async(req,res)=>{
-    try {
-        console.log(req.params);
-        const {id} = req.params;
-        const userindividual = await User.findById({_id:id});
-        console.log(userindividual);
-        res.status(201).json(userindividual)
-    } catch (error) {
-        res.status(500).send({ message: "Internal Server Error" });
-    }
-})
-*/
-router.get("/:id/verify/:token/", async (req, res) => {
-	try {
-		const user = await User.findOne({ _id: req.params.id });
-		if (!user) return res.status(400).send({ message: "Invalid link" });
-
-		const token = await Token.findOne({
-			userId: user._id,
-			token: req.params.token,
-		});
-		console.log(token);
-		if (!token) return res.status(400).send({ message: "Invalid link" });
-		console.log("a");
-		await User.findOneAndUpdate({ _id: user._id },{verified:true});
-
-		//await token.remove();
-
-		res.status(200).send({ message: "Email verified successfully" });
-	} catch (error) {
-		res.status(500).send({error });
-	}
-});
-//individual
-router.get("/getuser/:id",async(req, res) => {
-	try {
-        User.findById(req.params.id, (result, err) => {
-            if (err) {
-                res.json(err);
-            } else {
-                res.json(result.data);
-            }
-
-        });
-    } catch (err) {
-        res.json(err);
-    }
-
-  });
-
-  module.exports = router;
+module.exports = router;
