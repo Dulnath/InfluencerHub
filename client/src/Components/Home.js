9a2cdb2b13a905f81390b2024eb0c4a4df3cd1d9
Veenaka <lahiruveenaka@gmail.com>
2022-06-09 18:25:46 +0530

    Did UI changes

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,9 +1,10 @@
 import { React, useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Container, Col, Row } from "react-bootstrap";
 import CommentForm from "./CommentForm";
 import CommentList from "./CommentList";
+import styles from "./styles.module.css";
 
 function AllPosts() {
   const [listOfPosts, setListOfPosts] = useState([]);
@@ -45,9 +46,9 @@ function AllPosts() {
   let navigate = useNavigate();
 
   return (
-    <div className="container">
+    <div className={styles.background}>
       <h1>All posts</h1>
-      <button className="btn btn-success">
+      <button className={styles.btnGreen}>
         <a href="/add" style={{ textDecoration: "none", color: "white" }}>
           Create New Post
         </a>
@@ -55,30 +56,30 @@ function AllPosts() {
       {listOfPosts.map((posts) => {
         return (
           <div>
-            <Card className="PostdetailsCard" border="dark">
-              <div className="details">
-                <span className="title">Post Topic:</span>
-                <span className="data">{posts.PostTopic}</span>
-              </div>
-              <div className="details">
-                <span className="title">Post Description:</span>
-                <span className="data">{posts.Postdescription}</span>
-              </div>
-              <div className="details">
-                <span className="title">Post Image:</span>
-                <span className="data">
-                  {
-                    <img
-                      src={`${posts.PostImage}`}
-                      alt=""
-                      width="500"
-                      height="300"
-                      loading="eager"
-                    ></img>
-                  }
-                </span>
-              </div>
-              <div>
+            <Card className={styles.record}>
+              <Card.Header>
+                <b>{posts.PostTopic}</b>
+              </Card.Header>
+              <Card.Body>
+                <Row>
+                  <Card.Text as={Col}>{posts.Postdescription}</Card.Text>
+                </Row>
+                <br />
+                <Row>
+                  <Card.Text>
+                    {
+                      <img
+                        src={`${posts.PostImage}`}
+                        alt=""
+                        width="500"
+                        height="300"
+                        loading="eager"
+                      ></img>
+                    }
+                  </Card.Text>
+                </Row>
+                <br />
+
                 <Button
                   className="postButton2"
                   variant="warning"
@@ -90,6 +91,8 @@ function AllPosts() {
                 >
                   Edit Post
                 </Button>
+                <br />
+                <br />
                 <Button
                   className="postButton2"
                   variant="danger"
@@ -99,6 +102,8 @@ function AllPosts() {
                 >
                   Delete Post
                 </Button>
+                <br />
+                <br />
                 <Button
                   size="sm"
                   type="submit"
@@ -108,32 +113,39 @@ function AllPosts() {
                 >
                   Add Comment
                 </Button>
-              </div>
+              </Card.Body>
+              <Card.Footer>
+                {selected === posts._id
+                  ? openCommentForm && (
+                      <div>
+                        <CommentForm postID={posts._id} />
+                      </div>
+                    )
+                  : null}
+
+                {listOfComments.filter(
+                  (comment) =>
+                    comment.postId === posts._id && comment.isVisible === true
+                ).length > 0 ? (
+                  <p
+                    style={{
+                      fontWeight: "500",
+                      textDecorationLine: "underline",
+                    }}
+                  >
+                    <a onClick={() => viewComments(posts._id)}>View Comments</a>
+                  </p>
+                ) : null}
+
+                {selected === posts._id
+                  ? openComments && (
+                      <div>
+                        <CommentList postID={posts._id} />
+                      </div>
+                    )
+                  : null}
+              </Card.Footer>
             </Card>
-            {selected === posts._id
-              ? openCommentForm && (
-                  <div>
-                    <CommentForm postID={posts._id} />
-                  </div>
-                )
-              : null}
-
-            {listOfComments.filter(
-              (comment) =>
-                comment.postId === posts._id && comment.isVisible === true
-            ).length > 0 ? (
-              <p style={{ fontWeight: "500", textDecorationLine: "underline" }}>
-                <a onClick={() => viewComments(posts._id)}>View Comments</a>
-              </p>
-            ) : null}
-
-            {selected === posts._id
-              ? openComments && (
-                  <div>
-                    <CommentList postID={posts._id} />
-                  </div>
-                )
-              : null}
           </div>
         );
       })}
