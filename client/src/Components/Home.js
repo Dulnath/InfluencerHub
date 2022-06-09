0ea482f322d9f1d571f69fdded17d7286514a8da
Veenaka <lahiruveenaka@gmail.com>
2022-06-09 15:57:22 +0530

    Did a minor change in the layout

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
-import CommentForm from './CommentForm';
+import CommentForm from "./CommentForm";
 import CommentList from "./CommentList";
 
 function AllPosts() {
@@ -16,7 +16,7 @@ function AllPosts() {
     axios.get("http://localhost:5000/posts").then((response) => {
       setListOfPosts(response.data);
     });
-    axios.get('http://localhost:5000/getComments').then((response) => {
+    axios.get("http://localhost:5000/getComments").then((response) => {
       setListOfComments(response.data);
     });
   }, []);
@@ -35,18 +35,23 @@ function AllPosts() {
   const commentForm = (id) => {
     setSelected(id);
     setOpenCommentForm(!openCommentForm);
-  }
+  };
 
   const viewComments = (id) => {
     setSelected(id);
     setOpenComments(!openComments);
-  }
+  };
 
   let navigate = useNavigate();
 
   return (
     <div className="container">
       <h1>All posts</h1>
+      <button className="btn btn-success">
+        <a href="/add" style={{ textDecoration: "none", color: "white" }}>
+          Create New Post
+        </a>
+      </button>
       {listOfPosts.map((posts) => {
         return (
           <div>
@@ -97,37 +102,41 @@ function AllPosts() {
                 <Button
                   size="sm"
                   type="submit"
-                  onClick={() => { commentForm(posts._id) }}>Add Comment</Button>
+                  onClick={() => {
+                    commentForm(posts._id);
+                  }}
+                >
+                  Add Comment
+                </Button>
               </div>
             </Card>
-            {(selected === posts._id) ?
-              openCommentForm &&
-              < div >
-                <CommentForm postID={posts._id} />
-              </div> : null
-            }
+            {selected === posts._id
+              ? openCommentForm && (
+                  <div>
+                    <CommentForm postID={posts._id} />
+                  </div>
+                )
+              : null}
 
-            {listOfComments.filter((comment) => comment.postId === posts._id && comment.isVisible === true).length > 0 ?
+            {listOfComments.filter(
+              (comment) =>
+                comment.postId === posts._id && comment.isVisible === true
+            ).length > 0 ? (
               <p style={{ fontWeight: "500", textDecorationLine: "underline" }}>
                 <a onClick={() => viewComments(posts._id)}>View Comments</a>
-              </p> : null
-            }
+              </p>
+            ) : null}
 
-            {(selected === posts._id) ?
-              openComments &&
-              <div>
-                <CommentList postID={posts._id} />
-              </div> : null
-            }
-
+            {selected === posts._id
+              ? openComments && (
+                  <div>
+                    <CommentList postID={posts._id} />
+                  </div>
+                )
+              : null}
           </div>
         );
       })}
-      <button className="btn btn-success">
-        <a href="/add" style={{ textDecoration: "none", color: "white" }}>
-          Create New Post
-        </a>
-      </button>
     </div>
   );
 }
