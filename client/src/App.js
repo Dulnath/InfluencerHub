a2943497828ce310204b0999ceebd00f42611d49
Kumuthu Dulnath <kumuthudulnath@gmail.com>
2022-06-13 09:12:01 +0530

    more changes prior to merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CreatePost from "./Components/posts-and-comments/CreatePost";
-import EditPost from "./Components/posts-and-comments/EditPost";
-import Home from "./Components/posts-and-comments/Home";
-import PostDetails from "./Components/posts-and-comments/PostDetails";
+import CreatePost from "./components/posts-and-comments/CreatePost";
+import EditPost from "./components/posts-and-comments/EditPost";
+import Home from "./components/posts-and-comments/Home";
+import PostDetails from "./components/posts-and-comments/PostDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
