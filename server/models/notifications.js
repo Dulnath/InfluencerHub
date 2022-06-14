b8005717179d2310d5944511184ea2ca4ff99857
lahiruveenaka@gmail.com <lahiruveenaka@gmail.com>
2022-06-14 11:04:50 +0530

    Added Notifications schema

---
+++ server/models/notifications.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const NotificationSchema = new mongoose.Schema(
+  {
+    InvolvedInfluencerId: {
+      type: String,
+      required: true,
+    },
+    InvolvedBusinessId: {
+      type: String,
+      required: true,
+    },
+    Eventhappened: {
+      type: String,
+      required: true,
+    },
+    Notificationmessage: {
+      type: String,
+      required: true,
+    },
+    Seen: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Notification", NotificationSchema);
