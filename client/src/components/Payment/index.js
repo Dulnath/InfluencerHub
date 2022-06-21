3979cea8b132516e03ce17e33594a8dd8609be18
lahiruveenaka@gmail.com <lahiruveenaka@gmail.com>
2022-06-21 12:34:15 +0530

    Added Notifications for Payment

--- client/src/components/Payment/index.js
+++ client/src/components/Payment/index.js
@@ -1,20 +1,18 @@
-import MainMenu from '../Main/MainMenu';
+import MainMenu from "../Main/MainMenu";
 import axios from "axios";
 import React, { useState } from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom";
 import { useParams } from "react-router-dom";
 import ParseJwt from "../Utilities/ParseJwt";
 const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
+const Payment = () => {
+  const id = useParams();
 
-
-const Payment =() =>{
-   const id =useParams().id
-
-   const userToken = localStorage.getItem("token");
-  const user = ParseJwt(userToken);  
-const [doller, setDoller] = useState("");
-const [pypl, setPypl] = useState("");
+  const userToken = localStorage.getItem("token");
+  const user = ParseJwt(userToken);
+  const [doller, setDoller] = useState("");
+  const [pypl, setPypl] = useState("");
   const createOrder = (data, actions) => {
     return actions.order.create({
       purchase_units: [
@@ -23,73 +21,105 @@ const [pypl, setPypl] = useState("");
             value: doller,
           },
           payee: {
-            email_address: pypl
-          }
+            email_address: pypl,
+          },
         },
       ],
     });
   };
-  const onApprove =async (data, actions) => {
-   console.log(data);
-   const order = await actions.order.capture();
-   console.log(order);
-   console.log(id);
-   console.log("hello");
+  const onApprove = async (data, actions) => {
+    console.log(data);
+    const order = await actions.order.capture();
+    console.log(order);
+    console.log(id);
+    console.log("hello");
 
-   console.log(user._id);
-   axios.get(`http://localhost:5000/getProject/${id}`).then((response) => {
-    
+    console.log(user._id);
+    const userToken = localStorage.getItem("token");
+    const loggeduser = ParseJwt(userToken);
+    let NotificationTime = new Date().toLocaleString();
+    axios.get(`http://localhost:5000/getProject/${id}`).then((response) => {
+      console.log(response.data);
 
+      axios
+        .post("http://localhost:5000/createNotification", {
+          ReceiverId: response.data.project.influencerID,
+          SenderId: loggeduser._id,
+          Eventhappened: "Payment to a project",
+          NotificationTime,
+          Notificationmessage:
+            response.data.project.businessName +
+            " paid you for " +
+            response.data.project.projectName +
+            " project",
+        })
+        .then((res) => {
+          console.log("Notification created");
+          alert("Notification created successfully");
+        });
+
+      axios.post("http://localhost:5000/createPayment", {
+        amount: order.purchase_units[0].amount.value,
+        bid: user._id,
+        iid: response.data.project.influencerID,
+        paidto: response.data.project.influencerName,
+        paidby: response.data.project.businessName,
+        time: order.purchase_units[0].amount.value,
+        project: response.data.project.projectName,
+      });
+      console.log(response.data.project.influencerID);
+      axios
+        .get(
+          `http://localhost:5000/api/users/getuser/${response.data.project.influencerID}`
+        )
+        .then((response) => {
           console.log(response.data);
-          axios.post('http://localhost:5000/createPayment',{
-            amount:order.purchase_units[0].amount.value,
-            bid:user._id,
-            iid:response.data.project.influencerID,
-            paidto:response.data.project.influencerName,
-            paidby:response.data.project.businessName,
-            time:order.purchase_units[0].amount.value,
-            project:response.data.project.projectName,
-         })
-               console.log(response.data.project.influencerID);
-               axios.get(`http://localhost:5000/api/users/getuser/${response.data.project.influencerID}`).then((response)=>{
-                  console.log(response.data);
-               })
-         })
-      
-       
-      
-      
-    
+        });
+    });
 
-  
-   console.log(order.purchase_units[0].amount.value);
-  
+    console.log(order.purchase_units[0].amount.value);
 
-
-
-  
     return actions.order.capture();
-
-    
   };
- 
+
   return (
-   <div>
-   <MainMenu/>
-   <div style={{display:"flex",justifyContent:"center",flexDirection:"column",marginLeft:"450px",marginTop:"100px"}}>
- 
-   <div> <input type="text" value={doller}  onChange={(e)=>setDoller(e.target.value)} placeholder="Enter the amount"/></div>
-  <div> <input type="text" value={pypl}  onChange={(e)=>setPypl(e.target.value)} placeholder="Enter the influencers paypal email address"/></div>
-  <div>
-  <PayPalButton
-  createOrder={(data, actions) => createOrder(data, actions)}
-  onApprove={(data, actions) => onApprove(data, actions)}
-  
-/></div>
-  
-   </div></div>
-  
-  
+    <div>
+      <MainMenu />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          marginLeft: "450px",
+          marginTop: "100px",
+        }}
+      >
+        <div>
+          {" "}
+          <input
+            type="text"
+            value={doller}
+            onChange={(e) => setDoller(e.target.value)}
+            placeholder="Enter the amount"
+          />
+        </div>
+        <div>
+          {" "}
+          <input
+            type="text"
+            value={pypl}
+            onChange={(e) => setPypl(e.target.value)}
+            placeholder="Enter the influencers paypal email address"
+          />
+        </div>
+        <div>
+          <PayPalButton
+            createOrder={(data, actions) => createOrder(data, actions)}
+            onApprove={(data, actions) => onApprove(data, actions)}
+          />
+        </div>
+      </div>
+    </div>
   );
-}
+};
 export default Payment;
